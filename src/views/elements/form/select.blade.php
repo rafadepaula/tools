41600fa83ac2ed14828edb7fd6ac33f92a3dd536
Rafael de Paula <heeyrafa@gmail.com>
2020-10-18 20:38:00 -0300

    ajustes

--- src/views/elements/form/select.blade.php
+++ src/views/elements/form/select.blade.php
@@ -1,34 +1,34 @@
 <label for="{{$name}}">{{$label}} @if(!empty($atts['required']))* @endif</label>
 <select class="form-control {{$atts['class'] ?? ''}}" id="{{$atts['id'] ?? $name}}"
         name="{{$name}}@if(isset($atts['multiple']))[] @endif"
-@foreach($atts as $att => $valAtt)
-    @if($att != 'id' && $att != 'class')
-        {{$att}}="{{$valAtt}}"
-    @endif
-@endforeach >
-@foreach($options as $valueOpt => $label)
-    <option
+        @foreach($atts as $att => $valAtt)
+            @if($att != 'id' && $att != 'class')
+                {{$att}}="{{$valAtt}}"
+            @endif
+        @endforeach >
+    @foreach($options as $valueOpt => $label)
+        <option
             value="{{$valueOpt}}"
             @if(is_array(old($name)) && in_array($valueOpt, old($name)))
-            selected
+                selected
             @else
-            @if(is_array($value) && in_array($valueOpt, $value))
-            selected
-            @else
-            @if(!empty(old($name)) && old($name) == $valueOpt)
-            selected
-            @else
-            @if($valueOpt == $value)
-            selected
+                @if(is_array($value) && in_array($valueOpt, $value))
+                    selected
+                @else
+                    @if(!empty(old($name)) && old($name) == $valueOpt)
+                        selected
+                    @else
+                        @if($valueOpt == $value)
+                            selected
+                        @endif
+                    @endif
+                @endif
             @endif
-            @endif
-            @endif
-            @endif
-    >
-        {{$label}}
-    </option>
+        >
+            {{$label}}
+        </option>
     @endforeach
-    </select>
-    <div class="invalid-feedback">
-        <!-- ver erros !-->
-    </div>
+</select>
+<div class="invalid-feedback">
+    <!-- ver erros !-->
+</div>
